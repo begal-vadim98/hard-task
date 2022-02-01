04adfed831794e8d594f2c445f2c47cbc9b1b97d
Begal Wadim <begal-vadim98@yandex.ru>
2022-02-01 15:42:31 +0300

    Правки по дз 3 урока

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 'use strict'
 
 const myFunction = function (string) {
+  
   if(typeof string !== 'string') {
 
     return "В качестве аргумента передана не строка";
 
-  } else if(typeof string === 'string') {
+  } else if(string.length > 30) {
+
     string = string.trim();
-
-    if(string.length > 30) {
-
-      string = string.substring(0, 30) + "...";
-
-      return string;
-  
-    }
+    string = string.substring(0, 30) + "...";
 
     return string;
 
-  } 
+  } else {
+
+    return string;
+
+  }
   
 }
-console.log(myFunction("lorem  lorem lorem lorem lorem "));
+console.log(myFunction("loremloremloremloremloremloremlorem"));
