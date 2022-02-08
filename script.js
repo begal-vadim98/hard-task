2d9d9937b452783449ca2714288c83a4cc9e513c
Begal Wadim <begal-vadim98@yandex.ru>
2022-02-08 16:11:08 +0300

    Залил файлы дз 9 урока

--- script.js
+++ script.js
@@ -1,29 +1,96 @@
 'use strict'
+const timer = function () {
+  const boxA = document.querySelector('.box-fomat-a');
+  const boxB = document.querySelector('.box-fomat-b');
+    
+  const funcData = () => {
+    const date = new Date(),
+      year = date.getFullYear(),
+      monthNumeric = date.toLocaleString('ru', {
+        month: 'numeric'
+      }),
+      month = date.toLocaleString('ru', {
+        month: 'long'
+      }),
+      dateDayA = date.getDate(),
+      dateDayB = date.getDate(),
+      day = date.toLocaleString('ru', {
+        weekday: 'long'
+      }),
+      hours = date.getHours(),
+      minutes = date.getMinutes(),
+      seconds = date.getSeconds();
 
-const str = document.querySelectorAll('h3'),
-  box = document.querySelector('.box'),
-  week  = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-  date = new Date();
+    return {
+      'year': year,
+      'month': month,
+      'monthNumeric': +monthNumeric,
+      'dateDayA': dateDayA,
+      'dateDayB': dateDayB,
+      'day': day,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
 
-week.forEach((elem, index) => {
-  str[index].textContent = elem;
-  
-})
-box.style.cssText = ` height: 100vh;
-                          display: flex;
-                          flex-direction: column;
-                          align-items: center;`;
+ 
 
-str.forEach((elem, index) => {
-  elem.style.fontWeight = "300"
+  const changeDate = function () {
 
-    if(week[index] === "Суббота" || week[index] === "Воскресенье") {
-      elem.style.fontStyle = "italic"
-    } 
-    if(index === (date.getDay() - 1) || index === (date.getDay() + 6))  {
-      elem.style.fontWeight = "900"
-      elem.textContent =  "Сегодня у нас" + " " + elem.textContent 
+    const t = funcData();
+    t.month = t.month.slice(0, t.month.length - 1) + "я"
+    let stringHours = "часов";
+    let stringMinutes = "минут";
+    let stringSeconds = "секунд";
+    const funcDeclination = function() {
+
+      switch(true) {
+        case t.hours === 1 || t.hours === 21:
+          stringHours = "час";
+          break;
+        case 5 > t.hours && t.hours > 1 || 23 >= t.hours && t.hours > 21:
+          stringHours = "часа";
+          break;
+        default: 
+          stringHours = "часов";
+      }
+
+      switch(true) {
+        case t.minutes === 1 || t.minutes === 21 || t.minutes === 31 || t.minutes === 41 || t.minutes === 51:
+          stringMinutes = "минута";
+          break;
+        case t.minutes > 1 && t.minutes <= 4 || t.minutes > 21 && t.minutes <= 24 
+          || t.minutes > 31 && t.minutes <= 34 || t.minutes > 41 && t.minutes <= 44 
+          || t.minutes > 51 && t.minutes <= 54:
+          stringMinutes = "минуты";
+          break;
+      }
+      
+      switch(true) {
+        case t.seconds === 1 || t.seconds === 21 || t.seconds === 31 || t.seconds === 41 || t.seconds === 51:
+          stringSeconds = "секунда";
+          break;
+        case t.seconds > 1 && t.seconds <= 4 || t.seconds > 21 && t.seconds <= 24 
+          || t.seconds > 31 && t.seconds <= 34 || t.seconds > 41 && t.seconds <= 44 
+          || t.seconds > 51 && t.seconds <= 54:
+          stringSeconds = "секунды";
+          break;
+      }
+      
+      if(t.dateDayB < 10) t.dateDayB = "0" + t.dateDayB;
+      if(t.monthNumeric < 10) t.monthNumeric = "0" + t.monthNumeric;
+    
     }
-})
+    funcDeclination();
 
-console.log(date.getDay());
+    boxA.textContent = `Сегодня ${t.day},  ${t.dateDayA} ${t.month} ${t.year} года, ${t.hours} ${stringHours} ${t.minutes}  ${stringMinutes} ${t.seconds} ${stringSeconds}`;
+    boxB.textContent = `${t.hours}:${t.minutes}:${t.seconds} ${t.dateDayB}.${t.monthNumeric}.${t.year}`;
+    
+    let timerId = setInterval(changeDate, 1000);
+    
+  }
+  changeDate();
+
+}
+timer();
